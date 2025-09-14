50cd38e4dd3ee4e9c923ccd9bef09776b8a6133a
Maciej Siwak <maciej.siwak@deptagency.com>
2025-09-14 16:27:44 +0100

    updates

---
+++ metro.config.js
@@ -0,0 +1,14 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.assetExts = [
+  ...config.resolver.assetExts,
+  'png',
+  'jpg',
+  'jpeg',
+  'gif',
+  'webp',
+];
+
+module.exports = config;
