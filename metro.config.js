6b0dad20772c5ffe3b3c1da0fbce6862866f6d8f
Maciej Siwak <maciej.siwak@deptagency.com>
2025-09-14 17:13:55 +0100

    Updates

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,16 @@
+// metro.config.js
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+config.transformer = {
+  ...config.transformer,
+  assetPlugins: ['expo-asset/tools/hashAssetFiles'],
+};
+
 config.resolver.assetExts = [
   ...config.resolver.assetExts,
-  'png',
-  'jpg',
-  'jpeg',
-  'gif',
-  'webp',
+  'png', 'jpg', 'jpeg', 'gif', 'webp', 'svg', 'json'
 ];
 
 module.exports = config;
