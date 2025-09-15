6882c180219f135bdcc6ffd8572803eb6d35daa6
Maciej Siwak <maciej.siwak@deptagency.com>
2025-09-15 06:09:48 +0100

    Fixed

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,14 @@ module.exports = {
   presets: ['babel-preset-expo'],
   plugins: [
     'react-native-worklets/plugin',
+    [
+      'module-resolver',
+      {
+        alias: {
+          assets: './assets',
+        },
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+      },
+    ],
   ],
 };
--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,12 @@
-// metro.config.js
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-config.transformer = {
-  ...config.transformer,
-  assetPlugins: ['expo-asset/tools/hashAssetFiles'],
-};
+config.transformer.assetPlugins = ['expo-asset/tools/hashAssetFiles'];
 
 config.resolver.assetExts = [
   ...config.resolver.assetExts,
-  'png', 'jpg', 'jpeg', 'gif', 'webp', 'svg', 'json'
+  'png', 'jpg', 'jpeg', 'gif', 'webp', 'svg', 'json',
 ];
 
 module.exports = config;
